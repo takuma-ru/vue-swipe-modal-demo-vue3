57f28201cc6d7f9576e95bea3641300da62cdc34
takuma-ru <49429291+takuma-ru@users.noreply.github.com>
2022-09-12 11:38:13 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-dqxc2g
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-dqxc2g)
---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <button @click="open()">open modal</button>
+    <swipe-modal
+      v-model="modal"
+      contents-height="80vh"
+      border-top-radius="16px"
+      @close="close()"
+    >
+      <p>contents</p>
+    </swipe-modal>
+  </div>
+</template>
+
+<script setup lang="ts">
+import swipeModal from '@takuma-ru/vue-swipe-modal';
+import { ref } from 'vue';
+
+const modal = ref(false);
+
+const open = () => {
+  modal.value = true;
+};
+
+const close = () => {};
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
